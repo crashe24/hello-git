61accfe814c0161ef60924361dcad04095bc45d9
Jorge Leon <crashe24@hotmail.com>
2024-05-05 18:55:59 -0500

    last change

--- hellogit.js
+++ hellogit.js
@@ -8,4 +8,8 @@ console.log('hello worl')
 // git log para ver que se haya fotogragiado
 // git checkout [filename] se vuelve a la foto ultima tomada
 // git reset muestra los archivos modificados y que pueden ser fotografiados
-//
+// git log --graph
+// git log --graph --pretty=oneline
+// git log --graph --decorate --all oneline
+// git  config --global alias.tree "log --graph --decorate --all --oneline"
+// touch .gitignore
