e67619e242e072c3a07eaee3528d66f46fe9b1a0
Jorge Leon <crashe24@hotmail.com>
2024-05-05 17:54:03 -0500

    my second commit

--- hellogit.js
+++ hellogit.js
@@ -2,3 +2,7 @@ console.log('hello worl')
 // git init 
 // git branch -m main
 // git status
+// git add .
+// git status
+// git commit -m "my first commit"
+// git log para ver que se haya fotogragiado
