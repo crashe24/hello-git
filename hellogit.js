5d03ef43f22c3ee682e8f266914a2ee0c8ae2c7d
Jorge Leon <crashe24@hotmail.com>
2024-05-07 07:47:15 -0500

    new change

--- hellogit.js
+++ hellogit.js
@@ -39,7 +39,28 @@ console.log('hello worl')
 //git branch -M main
 //git push -u origin main
 // trabajar con git push y git pull
+// git pull y get fetch
+// git fetch es descargar el historial de cambios sin los cambios
+// git pull se descarga el historial con loscambios e historial
 
+// hint: You have divergent branches and need to specify how to reconcile them.
+// hint: You can do so by running one of the following commands sometime before
+// hint: your next pull:
+// hint:
+// hint:   git config pull.rebase false  # merge
+// hint:   git config pull.rebase true   # rebase
+// hint:   git config pull.ff only       # fast-forward only
+// hint:
+// hint: You can replace "git config" with "git config --global" to set a default
+// hint: preference for all repositories. You can also pass --rebase, --no-rebase,
+// hint: or --ff-only on the command line to override the configured default per
+// hint: invocation.
+// fatal: Need to specify how to reconcile divergent branches.
+// crashe24@Jorges-MBP git_course % git config pull.rebase false
+// crashe24@Jorges-MBP git_course % git pull origin main
+
+
+// git clone
 
 
 
